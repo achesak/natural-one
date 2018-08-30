dc343754c4925bab52c994b9a0582e0a6b0accbc
Adam Chesak <achesak@yahoo.com>
2018-08-30 19:31:04 -0400

    Version 3.1

--- naturalone.py
+++ naturalone.py
@@ -5,7 +5,7 @@
 ###############################################################################
 #
 # Natural One
-# Version 3.0
+# Version 3.1
 #
 # Natural One is a dice roller designed primarily for the Pathfinder
 # tabletop roll-playing game.
